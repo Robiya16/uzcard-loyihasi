514f9022505fb60ae62aaea683c072ab2501d17c
Robiya <abdusalomovarobiya23@gmail.com>
2024-12-09 15:06:08 +0500

    Birinchi commit

---
+++ main.py
@@ -0,0 +1,116 @@
+    # ism = float(input("Rayhona", "Zuhra", "Sabina", "Madina", "Sohiba", "Asadbek"))
+    # familya = float(input("Abdusalomova", "Gulomova", "Shokirova", "Sobirov", "Shermatov"))
+    # print(ism)
+    # print(familya)
+
+
+# a = int(input('1- son: '))
+# b = int(input('2- son: '))
+#
+# kvadratlar_yigindisi = 0
+#
+# for i in range(a, b + 1):
+#     kvadratlar_yigindisi += 2 ** i
+#     print(kvadratlar_yigindisi)
+
+# my_set.add(4)
+# print(my_set)  # Natija: {1, 2, 3, 4}
+#from doctest import script_from_examples
+
+# my_set = {1, 2, 3}
+# my_set.clear()
+# print(my_set)  # Natija: set()
+
+# my_set = {1, 2, 3}
+# new_set = my_set.copy()
+# print(new_set)  # Natija: {1, 2, 3}
+
+# my_set = {1, 2, 3}
+# my_set.discard(2)
+# print(my_set)  # Natija: {1, 3}
+
+# set1 = {1, 2, 3}
+# set2 = {2, 3, 4}
+# print(set1.intersection(set2))  # Natija: {2, 3}
+
+# set1 = {1, 2, 3}
+# set2 = {2, 3, 4}
+# set1.intersection_update(set2)
+# print(set1)  # Natija: {2, 3}
+
+
+# set1 = {1, 2, 3}
+# set2 = {4, 5, 6}
+# print(set1.isdisjoint(set2))  # Natija: True
+
+# my_set = {1, 2, 3}
+# elem = my_set.pop()
+# print(elem)  # Natija: (tasodifiy o'chirilgan element)
+# print(my_set)  # Natija: qolgani
+
+
+# my_set = {1, 2, 3}
+# my_set.remove(2)
+# print(my_set)  # Natija: {1, 3}
+
+# set1 = {1, 2, 3}
+# set2 = {2, 3, 4}
+# print(set1.symmetric_difference(set2))  # Natija: {1, 4}
+
+# set1 = {1, 2, 3}
+# set2 = {2, 3, 4}
+# set1.symmetric_difference_update(set2)
+# print(set1)  # Natija: {1, 4}
+
+
+# set1 = {1, 2, 3}
+# set2 = {3, 4, 5}
+# print(set1.union(set2))  # Natija: {1, 2, 3, 4, 5}
+
+# set1 = {1, 2, 3}
+# set2 = {3, 4, 5}
+# set1.update(set2)
+# print(set1)  # Natija: {1, 2, 3, 4, mat
+
+#                 MANfiy MUSBAT
+
+
+# son1 = int(input("Birinchi sonni kiriting: "))
+# son2 = int(input("Ikkinchi sonni kiriting: "))
+# son3 = int(input("Uchinchi sonni kiriting: "))
+#
+# if son1 == son2 == son3:
+#     print("Uchala son bir xil")
+# elif son1 > son2 > son3:
+#     print("eng katta son")
+# elif son2 < son1 < son3:
+#     print("kichik son")
+# else:
+#     print("Son kiritilmadi")
+
+
+# #            manfiy musbat
+# son = int(input('Son kiriting'))
+# if son < 0:
+#     print('manfiy son')
+# else:
+#     print('musbat son')
+
+    #
+    # a = 'Hello'
+    # b = 'World'
+    # c = a + b
+    # print(c)
+
+
+
+# matn = input('Matn kiriting:')
+# qanday = input('qanday holatda (katta, kichik,besh)')
+#
+# if qanday == 'katta':
+#     print(matn.upper())
+# elif qanday == 'kichik':
+#     print(matn.lower())
+# elif qanday == 'bosh':
+#     print(matn.title())
+# else:
